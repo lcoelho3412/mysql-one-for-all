489134a11c20bb615123d9aa64eeb899861db2f4
lcoelho3412 <leandrocoelho3@yahoo.com>
2022-10-05 11:33:27 -0300

    REQUISITO 4

---
+++ desafio4.sql
@@ -0,0 +1,8 @@
+SELECT
+	u.user_name AS usuario,
+  IF(MAX(YEAR(h.play_history)) >= '2021', 'Usuário ativo', 'Usuário inativo') AS status_usuario
+FROM SpotifyClone.user AS u
+INNER JOIN SpotifyClone.play_history AS h
+	ON u.iduser = h.iduser
+GROUP BY u.user_name
+ORDER BY u.user_name;
